4925d5a107311ea7b4e8fab5016c42b1c4fdee06
pierre-Olivier <pomorin@coveo.com>
2021-04-12 14:16:48 -0400

    feat: added test and validate 1st datebox
    Validate 1st datebox, Test the second one, but cypress-xpath is destroying the setup, this is to
    wait till setup is stable.

--- packages/e2eTesting/cypress/support/commands.js
+++ packages/e2eTesting/cypress/support/commands.js
@@ -1,1 +1,19 @@
 import '@testing-library/cypress/add-commands';
+
+
+Cypress.Commands.add('getMonthFromNumeralValue', (val, secondMonth) => {
+	var month = new Array();
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+	expect(val.text()).to.eql(month[secondMonth]);
+})
